15d2e10052a5754feb93339a137fce67f031c8f7
jfrumana <86309281+jfrumana@users.noreply.github.com>
2021-10-30 00:12:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function pic(){
+    document.getElementById("yaana").innerHTML="<img ondblclick='textback()' src='https://www.mother.ly/media-library/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8yMzU1MDAyMS9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTY2MjE1NDEwN30.tmNJ1OD4InbHuK8Vmn2rTRu8ZYSetSuNXKbFTvLhBLs/image.jpg?width=1245&quality=85&coordinates=0%2C54%2C0%2C55&height=700'>";
+}
+function textback(){
+    document.getElementById("yaana").innerHTML="<p class='mohsin'>mohsin maamu k paas my rootyu-yaana</p>";
+}
